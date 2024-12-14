09e9634db919382110e8ec61caf33083c1f588fe
Esdras Asa <esdrasasaoficial@gmail.com>
2024-12-14 12:45:55 -0300

    update

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const btn = document.getElementById("switch")
 const htmlElement = document.documentElement;
 let imageProfile = document.querySelector("#profile img")
+let nameProfile = document.getElementById("nameProfile")
+let items = document.querySelectorAll(".item")
+let content = document.getElementById("content")
 
 btn.addEventListener("click", switchMode)
+
+
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+
+    const content = item.querySelector("#content, #images, #plans");
+
+    if (content) {
+      content.style.display = content.style.display === "flex" ? "none" : "flex";
+    }
+  });
+});
+
 
 function switchMode() {
   htmlElement.classList.toggle("light")
 
   if (htmlElement.classList.contains("light")) {
     imageProfile.setAttribute("src", "./assets/avatar-light.png")
+    nameProfile.textContent = "Rebeka Thamilis";
   } else {
     imageProfile.setAttribute("src", "./assets/avatar.png")
+    nameProfile.textContent = "Esdras Asa";
   }
 }
